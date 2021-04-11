cd576dc2b0efcbee7ccfeb7aa5591c68943f85dc
Zulfadli77 <79710323+Zulfadli77@users.noreply.github.com>
2021-04-11 22:16:20 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+//---------------------------------For Navigation----------------------------------//
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+//----------------------------------------------Contact---------------------------------------------------//
+function btnSub(){
+    	alert("Sorry, not working");
+}
